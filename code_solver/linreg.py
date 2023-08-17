ac61fd9a087a32e44add11f59c6b63bd23fb4aad
philipschw <83816498+philipschw@users.noreply.github.com>
2023-08-17 20:01:02 +0200

    Update linreg.py
    Make linreg robust

--- code_solver/linreg.py
+++ code_solver/linreg.py
@@ -20,6 +20,23 @@ def linreg(y: np.ndarray, X: np.ndarray, intcpt: bool = True, nnconstraint: bool
     :return: A tuple consiting of the estimated coefficients of shape (p+1,), t-statistics of the coefficients of shape (p+1,), and the R-squared value of the regression model.
     :rtype: tuple
     """
+    if not isinstance(y, np.ndarray):
+        raise TypeError('y must be a numpy.ndarray')
+    if y.ndim != 1:
+        raise ValueError('y must be a 1-D array')
+    if not isinstance(X, np.ndarray):
+        raise TypeError('X must be a numpy.ndarray')
+    if X.ndim != 2:
+        raise ValueError('X must be a 2-D array')
+    if y.shape[0] != X.shape[0]:
+        raise ValueError('The number of rows in y and X must be the same')
+    if X.shape[0] < X.shape[1]:
+        raise ValueError('The number of rows in X must be greater than or equal to the number of columns')
+    if not isinstance(intcpt, bool):
+        raise TypeError('intcpt must be a boolean')
+    if not isinstance(nnconstraint, bool):
+        raise TypeError('nnconstraint must be a boolean')
+
     # Remove rows with NaN values
     mask = np.isnan(y) | np.isnan(X).any(axis=1)
     y = y[~mask]
@@ -43,12 +60,3 @@ def linreg(y: np.ndarray, X: np.ndarray, intcpt: bool = True, nnconstraint: bool
     ssres = np.sum(res ** 2)
     rsquared = 1 - (ssres / sstot)
 
-    # Calculate t-statistics
-    n = len(y)
-    p = X.shape[1] - 1
-    mse = ssres / (n - p - 1)
-    covmat = np.linalg.inv(X.T @ X) * mse
-    stderr = np.sqrt(np.diag(covmat))
-    tstat = coefficients / stderr
-
-    return coefficients, tstat, rsquared
